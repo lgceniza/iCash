0496b1a8fd66c04d7a3183f0041347306eb1abfa
Brandon Martin <bbmartin@up.edu.ph>
2018-11-03 03:37:22 +0800

    Positioned score text

--- interface.py
+++ interface.py
@@ -5,3 +5,14 @@ pygame.font.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_RESOLUTION = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+MEDIUM_FONT = pygame.font.Font("press-start.k.ttf", 50)
+
+BLACK = (0, 0, 0)
+MATRIX_GREEN = (30, 197, 3)
+
+def print_score(score, game_screen):
+    score_width, score_height = MEDIUM_FONT.size("$" + str(score))
+    position = [(SCREEN_WIDTH / 2) - (score_width / 2), (SCREEN_HEIGHT - score_height) - 25]
+    score = MEDIUM_FONT.render("$" + str(score), True, MATRIX_GREEN)
+    game_screen.blit(score, position)
--- main.py
+++ main.py
@@ -9,5 +9,9 @@ while not game_has_ended:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_has_ended = True
+    
+    interface.print_score(0, game_screen)
+
+    pygame.display.update()
 
 pygame.quit()
